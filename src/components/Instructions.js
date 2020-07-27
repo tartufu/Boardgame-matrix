82ac9369a7cb553210918b9c1c6322191b5e9f58
tartufu <ainurlindale@gmail.com>
2020-07-27 23:00:13 +0800

    instructions for 3P

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -11,6 +11,7 @@ function Instructions(props) {
                 <p> To use this web app, key in your BGG username and click submit or press enter.</p>
                 <p> Filter your gamelist according to your required players and game time by the sliders above.</p>
                 <p> Shuffle your current/filtered gamelist by clicking on the shuffle game button.</p> 
+                <p> For 3 players, please set filter at 4P unless you are looking for games that specifically plays only 3P.</p>
             </div>
         )
     } else {
